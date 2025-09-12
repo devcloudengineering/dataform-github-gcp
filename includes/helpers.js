4208d4dafda5ca3e6f01d9cfa01b7c921a49ab0e
gustavo.creado <gustavocreado1@gmail.com>
2025-09-12 15:33:57 +0000

    Modificamos variable de entorno dateProcess

--- includes/helpers.js
+++ includes/helpers.js
@@ -1,7 +1,6 @@
 // includes/helpers.js
-function procDate() {
-  // Devuelve un pedazo de SQL seguro con la fecha del var
-  const d = dataform.projectConfig.vars.proc_date; // p.ej. "2025-09-10"
+function dateProcess() {
+  const d = dataform.projectConfig.vars.fecha_proceso; 
   return `CAST(${JSON.stringify(d)} AS DATE)`;
 }
-module.exports = { procDate };
+module.exports = { dateProcess };
