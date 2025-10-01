4fcff21c52284a81cb151e18705b11b929089598
Gustavo <158855103+devcloudengineering@users.noreply.github.com>
2025-10-01 17:23:41 -0300

    Update sources.js

--- definitions/sources.js
+++ definitions/sources.js
@@ -1,19 +1,19 @@
 declare ({
-  database: "mi-primer-proyecto-469023",
+  database: "eng-archery-473819-h2",
   schema: "raw",
   name: "clientes_ext",
   description: "Clientes desde GCS (externa)"
 })
 
 declare ({
-  database: "mi-primer-proyecto-469023",
+  database: "eng-archery-473819-h2",
   schema: "raw",
   name: "productos_ext",
   description: "Productos desde GCS (externa)"
 })
 
 declare ({
-  database: "mi-primer-proyecto-469023",
+  database: "eng-archery-473819-h2",
   schema: "raw",
   name: "ventas_ext",
   description: "Ventas desde GCS (externa)"
